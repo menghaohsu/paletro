652f9651d067abf6194eb841b3fb6554fb6f80a8
Betty Bei <bettybei96@gmail.com>
2016-08-14 13:26:07 -0400

    moved previous 'home' state to 'editor' state

---
+++ browser/js/editor/editor.js
@@ -0,0 +1,82 @@
+
+app.config(function ($stateProvider) {
+    $stateProvider.state('editor', {
+        url: '/',
+        controller: 'EditorController',
+        templateUrl: 'js/editor/editor.html'
+    });
+});
+
+
+app.controller('EditorController', function ($scope, $rootScope, EditorFactory, ProjectFactory) {
+    $(".button-collapse").sideNav();
+    $('.collapsible').collapsible();
+    $scope.elements = [];
+    var obj = {}
+
+    $scope.addNavbar = function () {
+      $scope.elements.push({type: 'navbar'});
+      $('.button-collapse').sideNav('hide');
+    }
+
+    $scope.addLogo = function () {
+      $scope.elements.push({type: 'logo'});
+      $('.button-collapse').sideNav('hide');
+    }
+
+    $scope.addButton = function () {
+      $scope.elements.push({type: 'button'});
+      $('.button-collapse').sideNav('hide');
+    }
+
+    $scope.addDiv = function () {
+      $scope.elements.push({type: 'div'});
+      $('.button-collapse').sideNav('hide');
+    }
+
+    $scope.addImage = function () {
+      $scope.elements.push({type: 'image', url: $scope.image.url});
+      $('.button-collapse').sideNav('hide');
+    }
+
+    $scope.addText = function () {
+      $scope.elements.push({type: 'textbox'});
+      $('.button-collapse').sideNav('hide');
+    }
+
+    $scope.addProjectName = function () {
+      obj.name = $scope.projectName;
+    }
+
+    $scope.finished = function () {
+
+      var stringedElements =JSON.stringify($scope.elements)
+
+      obj.contents = stringedElements
+
+      // var p = document.getElementById("canvas");
+      // var pClone = p.cloneNode(true);
+      // console.log(pClone);
+      // console.log(p.innerHTML);
+      EditorFactory.delete(1)
+      .then(function(){
+        $scope.elements.map(element => EditorFactory.createElement(element))
+      })
+      .then(function(){
+        ProjectFactory.create(obj)
+      })
+    }
+
+    $scope.selectedColor = 'blue';
+    $scope.setColor = function (color) {
+      $scope.selectedColor = color;
+      $rootScope.$broadcast('colorChange', $scope.selectedColor)
+    }
+
+    $scope.setShade = function (shade) {
+      $scope.selectedShade = shade;
+      $rootScope.$broadcast('shadeChange', $scope.selectedShade)
+    }
+
+});
+
--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,4 +1,3 @@
-
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
@@ -8,76 +7,6 @@ app.config(function ($stateProvider) {
 });
 
 
-app.controller('HomeController', function ($scope, $rootScope, Home, ProjectFactory) {
-    $(".button-collapse").sideNav();
-    $('.collapsible').collapsible();
-    $scope.elements = [];
-    var obj = {}
-
-    $scope.addNavbar = function () {
-      $scope.elements.push({type: 'navbar'});
-      $('.button-collapse').sideNav('hide');
-    }
-
-    $scope.addLogo = function () {
-      $scope.elements.push({type: 'logo'});
-      $('.button-collapse').sideNav('hide');
-    }
-
-    $scope.addButton = function () {
-      $scope.elements.push({type: 'button'});
-      $('.button-collapse').sideNav('hide');
-    }
-
-    $scope.addDiv = function () {
-      $scope.elements.push({type: 'div'});
-      $('.button-collapse').sideNav('hide');
-    }
-
-    $scope.addImage = function () {
-      $scope.elements.push({type: 'image', url: $scope.image.url});
-      $('.button-collapse').sideNav('hide');
-    }
-
-    $scope.addText = function () {
-      $scope.elements.push({type: 'textbox'});
-      $('.button-collapse').sideNav('hide');
-    }
-
-    $scope.addProjectName = function () {
-    obj.name = $scope.projectName;
-    }
-
-    $scope.finished = function () {
-     
-      var stringedElements =JSON.stringify($scope.elements)
-      
-      obj.contents = stringedElements
-
- 
-      // var p = document.getElementById("canvas");
-      // var pClone = p.cloneNode(true);
-      // console.log(pClone);
-      // console.log(p.innerHTML);
-      Home.delete(1)
-      .then(function(){
-        $scope.elements.map(element=>Home.create(element))
-      })   
-      .then(function(){
-        ProjectFactory.create(obj)
-      })
-    }
-
-    $scope.selectedColor = 'blue';
-    $scope.setColor = function (color) {
-      $scope.selectedColor = color;
-      $rootScope.$broadcast('colorChange', $scope.selectedColor)
-    }
-
-    $scope.setShade = function (shade) {
-      $scope.selectedShade = shade;
-      $rootScope.$broadcast('shadeChange', $scope.selectedShade)
-    }
+app.controller('HomeController', function ($scope) {
 
 });
-
