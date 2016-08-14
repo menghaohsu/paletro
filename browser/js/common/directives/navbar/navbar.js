156749064ff5127ff60c89e19445b50c43a546f4
Betty Bei <bettybei96@gmail.com>
2016-08-14 17:24:30 -0400

    pimped oauth buttons with icons

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -7,9 +7,9 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
         link: function (scope) {
 
             scope.items = [
-                { label: 'Home', state: 'home' }, 
+                { label: 'Home', state: 'home' },
                 { label: 'About', state: 'about' },
-                { label: 'Projects', state: 'Projects' },
+                { label: 'Projects', state: 'projects' },
                 { label: 'Members Only', state: 'membersOnly', auth: true }
             ];
 
