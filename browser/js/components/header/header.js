4e565f794e6542e6380829f1a52e3559259127ab
Se Choong Kim <sck324@nyu.edu>
2016-08-16 14:53:09 -0400

    almost

--- browser/js/components/header/header.js
+++ browser/js/components/header/header.js
@@ -1,9 +1,7 @@
 
-
 app.directive('header', function () {
     return {
-        restrict: 'E',/*
-        controller: 'BoxController',*/
+        restrict: 'E',
         templateUrl: 'js/components/header/header.html',
         link: function (scope, elem, attr) {
           let ind = scope.$index;
@@ -11,31 +9,33 @@ app.directive('header', function () {
            scope.initialHeight = scope.$parent.elements[ind].height;
            scope.initialTop = scope.$parent.elements[ind].top;
            scope.initialLeft = scope.$parent.elements[ind].left;
- 
+           scope.initialFont = scope.$parent.elements[ind].fontsize;
+         
+         
           elem.draggable({
             stop: function (event, obj) {
               scope.$parent.elements[ind].top = scope.initialTop + obj.position.top;
               scope.$parent.elements[ind].left = scope.initialLeft + obj.position.left;
-              
-            }
+        
+          
+             }
           });
           angular.element(elem.find('h1')[0]).resizable({
             stop: function (event, obj) {
-              console.log('stopped resizing header', ind);
-              var element = elem.find('h1');
+             
+              var header = elem.find('h1');
               var size = elem.css("height");
-              (element).css("font-size", size.toString())
+
+              (header).css("font-size",size.toString()) //to make it bigger on the screen
+              scope.$parent.elements[ind].fontsize = size
+              scope.$parent.elements[ind].width = obj.size.width;
+              scope.$parent.elements[ind].height = obj.size.height;
+         
+
+              
+             
             }
           });
-          elem.find('h1').on('load', function(event){
-          elem.find('h1').resizable({
-            stop: function(event, obj) {
-              console.log('header resizing', ind)
-              scope.$parent.$parent.elements[ind].width = obj.size.width;
-              scope.$parent.$parent.elements[ind].height = obj.size.height;
-                    }
-                });
-            })
 
         }
     };
