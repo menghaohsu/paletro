fe4528482a76e9644650d4b791e245145385eb54
Betty Bei <bettybei96@gmail.com>
2016-08-17 17:27:04 -0400

    formatting

--- browser/js/renderCode/renderCode.js
+++ browser/js/renderCode/renderCode.js
@@ -7,17 +7,16 @@ app.config(function ($compileProvider,$stateProvider) {
         resolve: {
             templateCode: function(ProjectFactory,$stateParams){
                 var template = `<!DOCTYPE html>
-                                    <html lang="en">
-                                    <head>
-                                        <base href="/" />
-                                        <title>Testing</title>
-                                        <!-- Materialize.css-->
-                                        <link href="http://fonts.googleapis.com/icon?family=MaterialIcons" rel="stylesheet">
-                                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
-                                        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-                                        <script type="text/javascript" src="/materialize-css/dist/js/materialize.min.js"></script>
-                                    </head>
-                                    <body>`;
+<html lang="en">
+    <head>
+        <base href="/" />
+        <title>Page Title Here</title>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+    </head>
+    <body>`;
                 return ProjectFactory.getAllElements($stateParams.id)
                 .then(function(res){
                     res[0].elements.forEach(function(element){
@@ -25,41 +24,41 @@ app.config(function ($compileProvider,$stateProvider) {
                     })
                 })
                 .then(function(){
-                    template+=`</body>
-                    </html>`;
+                    template+=`
+    </body>
+</html>`;
                     return template
                 })
 
                 function renderCode(element){
                     if (element.type==='button') {
-                        template+=`<button class="btn
-                                    ${element.color} ${element.shade}" style="height:
-                                    ${element.height}px; width:${element.width}px; position: absolute; left:
-                                    ${element.left}px; top: ${element.top}px;">button</button>`;
+                        template+=`
+        <button class="btn ${element.color} ${element.shade}" style="position: absolute; height: ${element.height}px; width:${element.width}px; top: ${element.top}px; left: ${element.left}px;">button</button>`;
                     } else if (element.type==='textbox') {
-                        template+=`<textarea readonly style="height:
-                                    ${element.height}px; width:${element.width}px; position: absolute; left:
-                                    ${element.left}px; top: ${element.top}px; resize:none; border-color: white">${element.content}</textarea>`;
+                        template+=`
+        <textarea readonly style="position: absolute; height: ${element.height}px; width:${element.width}px; top: ${element.top}px; left: ${element.left}px; resize: none; border-color: white">${element.content}</textarea>`;
                     } else if (element.type==='div') {
-                        template+=`<div style="height:
-                                    ${element.height}px; width:${element.width}px; position: absolute; left:
-                                    ${element.left}px; top: ${element.top}px; border: 1px solid black"></div>`;
+                        template+=`
+        <div style="position: absolute; height: ${element.height}px; width:${element.width}px; top: ${element.top}px; left: ${element.left}px; border: 1px solid black"></div>`;
                     } else if (element.type==='image') {
-                        template+=`<img src="${element.url}" style="height:
-                                    ${element.height}px; width:${element.width}px; position: absolute; left:
-                                    ${element.left}px; top: ${element.top}px;">`;
+                        template+=`
+        <img src="${element.url}" style="position: absolute; height: ${element.height}px; width:${element.width}px; top: ${element.top}px; left: ${element.left}px;">`;
                     } else if (element.type==='logo') {
-                        template+=`<img src="https://jlau-bucket-1.s3.amazonaws.com/uploads/topic/image/42/fullstack.png" style="height:
-                                    ${element.height}px; width:${element.width}px; position: absolute; left:
-                                    ${element.left}px; top: ${element.top}px;">`;
+                        template+=`
+        <img src="https://jlau-bucket-1.s3.amazonaws.com/uploads/topic/image/42/fullstack.png" style="position: absolute; height: ${element.height}px; width:${element.width}px; top: ${element.top}px; left: ${element.left}px;">`;
                     } else if (element.type==='navbar') {
-                        template+=`<nav class="${element.color} ${element.shade}"><div class="nav-wrapper container"><a class="brand-logo">Sample Navbar</a>
-                                    <ul id="nav-mobile" class="right hide-on-med-and-down">
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">About</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                    <li><a href="#">Login</li></a></li>
-                                    </ul></div></nav>`;
+                        template+=`
+        <nav class="${element.color} ${element.shade}">
+            <div class="nav-wrapper container">
+                <a class="brand-logo">Sample Navbar</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Login</li></a></li>
+                </ul>
+            </div>
+        </nav>`;
                     }
                 }
             }
@@ -74,4 +73,8 @@ app.controller('RenderCodeCtrl', function($scope,$stateParams,$window,templateCo
     var blob = new Blob([ templateCode ], { type : 'html/HTML' });
     var url = $window.URL || $window.webkitURL;
     $scope.fileUrl = url.createObjectURL(blob);
+
+    $scope.copyCode = function () {
+        templateCode.createTextNode().execCommand('copy');
+    }
 })
