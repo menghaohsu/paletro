1059c76b60187ff18088733040dc14009807c308
Se Choong Kim <sck324@nyu.edu>
2016-08-21 16:38:24 -0400

    user-div refactored

--- browser/js/components/user-div/user-div.js
+++ browser/js/components/user-div/user-div.js
@@ -1,10 +1,15 @@
 app.directive('userDiv', function () {
     return {
-        restrict: 'E',
+         restrict: 'E',
+         scope: {
+          index: '=',
+          elements: '=',
+          dimension: '='
+          },
         templateUrl: 'js/components/user-div/user-div.html',
         link: function (scope, elem, attr) {
-          let ind = scope.$index;
-          let elemObj = scope.$parent.elements[ind];
+          let idx = scope.index
+          let elemObj = scope.elements[idx];
           scope.initialWidth = elemObj.width;
           scope.initialHeight = elemObj.height;
           scope.initialTop = elemObj.top;
@@ -15,9 +20,8 @@ app.directive('userDiv', function () {
           })
 
           elem.draggable({
-            grid: [scope.$parent.dimension, scope.$parent.dimension],
+            grid: [scope.dimension, scope.dimension],
             stop: function(event, obj) {
-              console.log("stopped dragging div", ind);
               elemObj.top = scope.initialTop + obj.position.top;
               elemObj.left = scope.initialLeft + obj.position.left;
               if(elemObj.top<-45&&elemObj.left>1070){
@@ -28,7 +32,6 @@ app.directive('userDiv', function () {
           });
           angular.element(elem.find('div')[0]).resizable({
             stop: function(event, obj) {
-              console.log("stopped resizing div", ind);
               elemObj.width = obj.size.width;
               elemObj.height = obj.size.height;
             }
